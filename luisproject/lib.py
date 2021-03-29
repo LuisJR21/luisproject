10e7ee4b4de9b3a3b57b370ea6f06a9c0f871246
Luis Jimenez <luis.jimenez@kraftheinz.com>
2021-03-29 14:12:27 -0600

    update

---
+++ luisproject/lib.py
@@ -0,0 +1,6 @@
+def try_me():
+    print ("This is a function to calculate the area of a circle")
+    print("Insert the radius of the circle")
+    r =float(input())
+    return 3.1415*(r**2)
+    
